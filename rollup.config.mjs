45bd0e2facf5c92ccd5e4a620658dcf272cc4704
Dawson Walker <dawsonwalker91@gmail.com>
2026-05-21 10:46:40 -0600

    chore(cleanup): cleanup rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -27,7 +27,9 @@ export default [
     external: [
       '@grpc/grpc-js',
       'google-protobuf',
+      '@miradorlabs/plugins',
       'mirador-gateway-ingest',
+      /^mirador-gateway-ingest\/.*/,
       'rxjs',
       /^rxjs\/.*/,
     ],
@@ -43,7 +45,9 @@ export default [
     external: [
       '@grpc/grpc-js',
       'google-protobuf',
+      '@miradorlabs/plugins',
       'mirador-gateway-ingest',
+      /^mirador-gateway-ingest\/.*/,
       'rxjs',
       /^rxjs\/.*/,
     ],
